2b745e0db0820f62648d4785dc971541f596fdeb
Tom Dyer <tdyer@ourstage.com>
2014-04-22 14:52:34 -0400

    Convert tabs to 2 spaces

--- students.rb
+++ students.rb
@@ -1,38 +1,38 @@
 class School
-	attr_accessor :students, :teachers
+  attr_accessor :students, :teachers
 
-	def initialize
-		@students = []
-		@teachers = []
-	end
+  def initialize
+    @students = []
+    @teachers = []
+  end
 end
 
 class Human
-	attr_accessor :name
+  attr_accessor :name
 
-	def initialize(name, age)
-		@name = name
-	end
+  def initialize(name, age)
+    @name = name
+  end
 end
 
 # Student inherits all attributes and methods from Human
 class Student < Human
-	attr_accessor :gpa
+  attr_accessor :gpa
 
-	# Overloads initialize method from Human with a new one
-	def initialize(name, age, gpa)
-		# super calls the initialize method of parent class
-		super(name, age) # needs to be one first line of initialize
-		@gpa = gpa
-	end
+  # Overloads initialize method from Human with a new one
+  def initialize(name, age, gpa)
+    # super calls the initialize method of parent class
+    super(name, age) # needs to be one first line of initialize
+    @gpa = gpa
+  end
 end
 
 class Teacher < Human
-	attr_accessor :students
-	def initialize(name, age, salary)
-		super(name, age)
-		@salary = salary
-	end
+  attr_accessor :students
+  def initialize(name, age, salary)
+    super(name, age)
+    @salary = salary
+  end
 end
 
 my_school = School.new
--- vehicle.rb
+++ vehicle.rb
@@ -1,34 +1,34 @@
 require 'pry'
 
 class Vehicle
-	attr_accessor :color, :number_of_wheels
+  attr_accessor :color, :number_of_wheels
 
-	def drive
-		puts "woooo wooooooo"
-	end
+  def drive
+    puts "woooo wooooooo"
+  end
 end
 
 class Motorcycle < Vehicle
-	attr_accessor :number_of_wheelies
+  attr_accessor :number_of_wheelies
 
-	def initialize(color)
-		@number_of_wheelies = 0
-		@color = color
-		@number_of_wheels = 2
-	end
+  def initialize(color)
+    @number_of_wheelies = 0
+    @color = color
+    @number_of_wheels = 2
+  end
 
-	def pop_a_wheelie
-		puts "Looks sweet"
-		@number_of_wheelies = @number_of_wheelies + 1
-	end
+  def pop_a_wheelie
+    puts "Looks sweet"
+    @number_of_wheelies = @number_of_wheelies + 1
+  end
 end
 
 class Car < Vehicle
-	attr_accessor :number_of_comfortable_seats
+  attr_accessor :number_of_comfortable_seats
 
-	def be_sat_in
-		puts "wow, this is boring"
-	end
+  def be_sat_in
+    puts "wow, this is boring"
+  end
 end
 
 my_motorcycle = Motorcycle.new("red")
