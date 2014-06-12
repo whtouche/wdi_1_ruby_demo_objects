73215a3835a56c5087cdd8c070581a3345b5cffe
Ben White <whtouche@gmail.com>
2014-06-12 15:57:10 -0400

    created person and song apps

--- bin/person_app.rb
+++ bin/person_app.rb
@@ -1,2 +1,30 @@
 # This will "insert" the contents of the person.rb here
 require_relative '../lib/person.rb'
+
+tom = Person.new('Tom', 'Dyer', '06-02-1962')
+jill = Person.new('Jill', 'Stein', '05-13-1987')
+
+puts "tom's first name is #{tom.first_name}" # Calling the Getter method
+puts "jill's last name is #{jill.last_name}" # Calling the Getter method
+
+# Use setter method to change tom's last name
+# Works even with parentheses
+tom.last_name = "Jones"
+
+# Show me tom's last name
+puts "tom's last name is #{tom.last_name}"
+
+# Shows address for the object tom
+puts tom
+
+# .inspect returns class name, instance variables (good for debugging)
+puts tom.inspect
+
+puts "tom's full name is #{tom.full_name}"
+puts "tom's age is #{tom.age}"
+
+tom.current_smoker = true
+tom.married = true
+msg = tom.give_insurance? ? "should" : "should not"
+puts "tom #{msg} get insured"
+
--- bin/song_app.rb
+++ bin/song_app.rb
@@ -4,3 +4,40 @@ require_relative '../lib/song.rb'
 
 # Add code to use the Song class here.
 
+rap = Song.new('Harder Than You Think', 'Public Enemy', 240, 3)
+rock = Song.new('Stairway to Heaven', 'Led Zeppelin', 360, 2)
+country = Song.new('Something Terrible', 'Someone Terrible', 999, 1)
+
+def average_duration(d1, d2, d3)
+  (d1 + d2 + d3) / 3
+end
+
+def average_price(p1, p2, p3)
+  (p1 + p2 + p3) / 3
+end
+
+puts rap.inspect
+puts rock.inspect
+puts country.inspect
+
+puts
+
+puts "The average duration is #{average_duration(rap.duration, rock.duration, country.duration)}"
+puts "The average cost is #{average_price(rap.price, rock.price, country.price)}"
+
+puts
+
+rap.duration = 360
+rock.duration = 460
+country.duration = 50
+
+puts rap.inspect
+puts rock.inspect
+puts country.inspect
+
+puts
+
+puts "The average duration is #{average_duration(rap.duration, rock.duration, country.duration)}"
+puts "The average cost is #{average_price(rap.price, rock.price, country.price)}"
+
+puts
